b266aa846bf6b1dd73d7b0904d40d5b0131e27ec
roee030 <roee.angel93@gmail.com>
2020-11-15 16:46:49 +0200

    refactor code

--- create.js
+++ create.js
@@ -1,13 +1,9 @@
 import * as uuid from 'uuid';
-import AWS from 'aws-sdk';
-// AWS.config.update({ region: 'US East' });
+import handler from './libs/handler-lib';
+import dynamoDb from './libs/dynamodb-lib';
 
-const dynamoDb = new AWS.DynamoDB.DocumentClient();
-
-export async function main(event, context) {
-	// Request body is passed in as a JSON encoded string in 'event.body'
+export const main = handler(async (event, context) => {
 	const data = JSON.parse(event.body);
-
 	const params = {
 		TableName: process.env.tableName,
 		Item: {
@@ -20,17 +16,7 @@ export async function main(event, context) {
 		},
 	};
 
-	try {
-		await dynamoDb.put(params).promise();
+	await dynamoDb.put(params);
 
-		return {
-			statusCode: 200,
-			body: JSON.stringify(params.Item),
-		};
-	} catch (e) {
-		return {
-			statusCode: 500,
-			body: JSON.stringify({ error: e.message }),
-		};
-	}
-}
+	return params.Item;
+});
