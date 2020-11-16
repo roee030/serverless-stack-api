6763874606b7a9dcb7c3babe9e30a47f5999c5eb
roee030 <roee.angel93@gmail.com>
2020-11-16 14:22:57 +0200

    test api

--- list.js
+++ list.js
@@ -3,7 +3,7 @@ import dynamoDb from './libs/dynamodb-lib';
 
 export const main = handler(async (event, context) => {
 	const params = {
-		TableName: 'notes',
+		TableName: process.env.tableName,
 		// 'KeyConditionExpression' defines the condition for the query
 		// - 'userId = :userId': only return items with matching 'userId'
 		//   partition key
