8bd73afec6505f5838fc82d4062f1e28ba183d44
roee030 <roee.angel93@gmail.com>
2020-11-16 12:37:03 +0200

    Users and authentication

--- create.js
+++ create.js
@@ -5,10 +5,10 @@ import dynamoDb from './libs/dynamodb-lib';
 export const main = handler(async (event, context) => {
 	const data = JSON.parse(event.body);
 	const params = {
-		TableName: process.env.tableName,
+		TableName: 'notes',
 		Item: {
 			// The attributes of the item to be created
-			userId: '123', // The id of the author
+			userId: event.requestContext.identity.cognitoIdentityId, // The id of the author // The id of the author
 			noteId: uuid.v1(), // A unique uuid
 			content: data.content, // Parsed from request body
 			attachment: data.attachment, // Parsed from request body
